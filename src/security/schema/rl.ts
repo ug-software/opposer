3f7d940987a91ddbdfd6d347c49a11474feae595
Uiaran Guilherme Campos de Lima <uiaran@hotmail.com>
2025-10-24 16:36:00 -0300

    bug/removed reference schema in schemas the secutiry module

--- src/security/schema/rl.ts
+++ src/security/schema/rl.ts
@@ -1,11 +1,10 @@
 import { Column, Entity, ManyToOne } from "typeorm";
-import Schema from "../../database/schema.js";
 import f from "../../database/field.js";
 import { Field } from "../../decorators/index.js";
 import User from "./usr.js";
 
 @Entity("rl")
-export default class Role extends Schema {
+export default class Role {
   @Column({ type: "varchar" })
   @Field(() => f().string("schema is string.").required("schema is required."))
   sm!: string;
