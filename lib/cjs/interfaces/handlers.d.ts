e279b1db4a7b2b481c7702c07cf2f87e410f9fd2
Uiaran Guilherme Campos de Lima <uiaran@hotmail.com>
2025-10-25 16:38:45 -0300

    bog/dispatch controll

---
+++ lib/cjs/interfaces/handlers.d.ts
@@ -0,0 +1,16 @@
+import { ClassType } from "./system.js";
+export interface ResultGetAllHandlers {
+    [key: string]: {
+        metadata: {
+            name: string;
+        };
+        methods: {
+            name: string;
+        }[];
+        handler: ClassType<any>;
+    };
+}
+export interface RequestHandlerBody {
+    method: string;
+    paylod: any;
+}
