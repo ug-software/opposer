a66c0642c43b68b89d6e8f9538ae058cb4f560e1
Uiaran Guilherme Campos de Lima <uiaran@hotmail.com>
2025-10-27 10:27:24 -0300

    fix/Re export organization

---
+++ lib/esm/server/security/schema/se.d.ts
@@ -0,0 +1,10 @@
+export default class Session {
+    id: string;
+    usr: string;
+    loi: Date;
+    lou: Date;
+    ac: boolean;
+    ip: string;
+    ag: string;
+    rt: string;
+}
--- src/persistent/context/index.ts
+++
@@ -1,5 +0,0 @@
-import { AsyncLocalStorage } from "async_hooks";
-
-const context = new AsyncLocalStorage();
-
-export default context;
