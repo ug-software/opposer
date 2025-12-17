5f7cf10040919a4fc78990d71cd29b7dad2a0e8c
Uiaran Guilherme Campos de Lima <uiaran@hotmail.com>
2025-12-17 17:00:14 -0300

    Fix correction in regex for validation password

--- src/playground/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Drawer } from "./drawer";
-export { default as SplitPane } from "./split-pane";
-export { default as JsonEditor } from "./json-editor";
-export { default as SplitPaneItem } from "./split-pane-item";
