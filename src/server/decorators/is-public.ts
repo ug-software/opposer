a90bdff95515e33255283c97018a04a4d1826317
Uiaran Guilherme Campos de Lima <uiaran@hotmail.com>
2025-12-17 15:38:23 -0300

    fix/correction decorator for public class

--- src/server/decorators/is-public.ts
+++ src/server/decorators/is-public.ts
@@ -2,16 +2,12 @@ import "reflect-metadata";
 
 const PUBLIC_KEY = Symbol("is-public");
 
-export default function Method() {
-  return function (target: Object, name: string, context: any) {
-    let _public = Reflect.getMetadata(PUBLIC_KEY, target.constructor) || false;
-
-    _public = true;
-
-    Reflect.defineMetadata(PUBLIC_KEY, _public, target.constructor);
+export function IsPublic(): ClassDecorator {
+  return (target) => {
+    Reflect.defineMetadata(PUBLIC_KEY, true, target);
   };
 }
 
-export function getIsPublicMetadata(target: Object) {
-  return Reflect.getMetadata(PUBLIC_KEY, target) || [];
+export function getIsPublicMetadata(target: Object): boolean {
+  return Reflect.getMetadata(PUBLIC_KEY, target) === true;
 }
