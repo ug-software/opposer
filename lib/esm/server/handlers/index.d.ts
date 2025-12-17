19e78ca341ff9483758e7912b2922a4c286aff05
Uiaran Guilherme Campos de Lima <uiaran@hotmail.com>
2025-12-17 16:27:09 -0300

    add is public decorator for methods

---
+++ lib/esm/server/handlers/index.d.ts
@@ -0,0 +1,4 @@
+import { ResultGetAllHandlers } from "../../interfaces/controller.js";
+export declare function toKebabCase(str: string): string;
+export declare function toCamelCase(str: string): string;
+export declare function loadHandlers(): Promise<ResultGetAllHandlers>;
