cd440d8fc4e64a272860de1c15f19aa9abddaf03
Uiaran Guilherme Campos de Lima <uiaran@hotmail.com>
2025-12-17 15:40:47 -0300

    Fix inplementation in add is public decorator for models

---
+++ lib/cjs/server/index.d.ts
@@ -0,0 +1,7 @@
+import { CreateServerProps, ServerInstance } from "../interfaces/server.js";
+export default function Server(props: CreateServerProps): Promise<ServerInstance>;
+export * from "./database/index.js";
+export * from "./constants/index.js";
+export * from "./helpers/index.js";
+export type { SchemaResult } from "../interfaces/schema.js";
+export { Method, Handler, Field, Payload, IsPublic, } from "./decorators/index.js";
---
+++ lib/esm/persistent/decorators/session.d.ts
@@ -0,0 +1,1 @@
+export default function Session(): (target: any, property: string) => void;
