1ab8e33222ad6109b0ec5c224ffa192068492dd5
Uiaran Guilherme Campos de Lima <uiaran@hotmail.com>
2025-12-19 14:58:24 -0300

    Exported security model user and role for external management

---
+++ lib/cjs/interfaces/server.d.ts
@@ -0,0 +1,14 @@
+import http from "http";
+import * as core from "express-serve-static-core";
+export interface CreateServerProps {
+    cors?: {
+        origin: string;
+    };
+}
+export interface ServerInstance {
+    opposer: core.Express;
+    initialize: () => void;
+}
+export type Server = http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>;
+export type HandleCallback = (req: http.IncomingMessage, res: http.ServerResponse) => void;
+export type ListinerCallback = () => void;
---
+++ lib/esm/server/security/schema/ke.d.ts
@@ -0,0 +1,6 @@
+export default class Key {
+    id: string;
+    hs: string;
+    ct: Date;
+    ex: Date;
+}
