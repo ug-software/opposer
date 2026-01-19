cdd9f1038521cb78c0529d4f4dee3d9655b67c27
Uiaran Guilherme Campos de Lima <uiaran@hotmail.com>
2026-01-19 09:59:43 -0300

    fix/correction in query tools, on in render object not including string values

--- src/server/tools/query.ts
+++ src/server/tools/query.ts
@@ -20,65 +20,65 @@ export default class QueryTool {
     Object.keys(query).forEach((key) => {
       if (typeof key !== "object") {
         __query[key] = query[key];
+      } else {
+        var __part: { [key: string]: any } = {};
+        Object.entries(query[key]).forEach(([key, value]) => {
+          switch (key) {
+            case "$or":
+              __part[key] = Or(value);
+              break;
+
+            case "$l":
+              __part[key] = Like(value);
+              break;
+
+            case "$il":
+              __part[key] = ILike(value);
+              break;
+
+            case "$in":
+              __part[key] = In(value);
+              break;
+
+            case "$nin":
+              __part[key] = Not(In(value));
+              break;
+
+            case "$btw":
+              if (!Array.isArray(value)) {
+                throw new Error(
+                  "[database] - for using between in query necessary value is Array."
+                );
+              }
+
+              var [first, second] = value;
+              __part[key] = Between(first, second);
+              break;
+
+            case "$mt":
+              __part[key] = MoreThan(value);
+              break;
+
+            case "$mte":
+              __part[key] = MoreThanOrEqual(value);
+              break;
+
+            case "$lt":
+              __part[key] = LessThan(value);
+              break;
+
+            case "$lte":
+              __part[key] = LessThanOrEqual(value);
+              break;
+
+            case "$eq":
+              __part[key] = Equal(value);
+              break;
+          }
+        });
+
+        __query[key] = __part;
       }
-
-      var __part: { [key: string]: any } = {};
-      Object.entries(query[key]).forEach(([key, value]) => {
-        switch (key) {
-          case "$or":
-            __part[key] = Or(value);
-            break;
-
-          case "$l":
-            __part[key] = Like(value);
-            break;
-
-          case "$il":
-            __part[key] = ILike(value);
-            break;
-
-          case "$in":
-            __part[key] = In(value);
-            break;
-
-          case "$nin":
-            __part[key] = Not(In(value));
-            break;
-
-          case "$btw":
-            if (!Array.isArray(value)) {
-              throw new Error(
-                "[database] - for using between in query necessary value is Array."
-              );
-            }
-
-            var [first, second] = value;
-            __part[key] = Between(first, second);
-            break;
-
-          case "$mt":
-            __part[key] = MoreThan(value);
-            break;
-
-          case "$mte":
-            __part[key] = MoreThanOrEqual(value);
-            break;
-
-          case "$lt":
-            __part[key] = LessThan(value);
-            break;
-
-          case "$lte":
-            __part[key] = LessThanOrEqual(value);
-            break;
-
-          case "$eq":
-            __part[key] = Equal(value);
-            break;
-        }
-      });
-
-      __query[key] = __part;
     });
 
     return __query;
