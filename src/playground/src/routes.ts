0ba4f6f8649c741122a0b36dff5a2909ba002112
Uiaran Guilherme Campos de Lima <uiaran@hotmail.com>
2026-02-20 16:31:36 -0300

    feat/created cookie session for more precision in auth routes

--- src/playground/src/routes.ts
+++
@@ -1,5 +0,0 @@
-import { type RouteConfig, index, layout } from "@react-router/dev/routes";
-
-export default [
-  layout("layouts/main/index.tsx", [index("pages/playground/index.tsx")]),
-] satisfies RouteConfig;
