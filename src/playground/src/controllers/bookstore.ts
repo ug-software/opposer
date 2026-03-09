96d3f1101101ea7e5dfe3bf0cfc039a2e4ce8017
Uiaran Guilherme Campos de Lima <uiaran@hotmail.com>
2026-03-09 11:00:12 -0300

    feat/add test files

--- src/playground/src/controllers/bookstore.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Method } from "opposer";
-
-@Controller('book-store')
-export default class BookStore {
-    @Method()
-    sincronize() {
-        return "Sincronizado."
-    }
-}
